f3a2f510b89139e112e530b22d052a21883a4c79
Michael Ikemann <michael.ikemann@gmail.com>
2026-03-22 13:51:44 +0100

    Initial package: document plugin system with MCP tools

---
+++ llming_docs/__init__.py
@@ -0,0 +1,42 @@
+"""Document plugin system for llming-lodge chat.
+
+Provides a plugin architecture for rich document creation and visualization
+within chat conversations. Supports Plotly charts, LaTeX formulas, tables,
+text documents, presentations, and HTML sandboxes.
+"""
+
+from llming_docs.document_store import Document, DocumentSessionStore
+from llming_docs.manager import DocPluginManager, ALL_DOC_PLUGIN_TYPES
+from llming_docs.render import (
+    EmbedBehavior,
+    EMBED_BEHAVIOR,
+    RenderResult,
+    RenderContext,
+    RENDER_CAPABILITIES,
+    EMBED_RULES,
+    render_to,
+    can_render,
+    can_embed,
+    get_embed_format,
+    get_embed_behavior,
+    register_embed_behavior,
+)
+
+__all__ = [
+    "Document",
+    "DocumentSessionStore",
+    "DocPluginManager",
+    "ALL_DOC_PLUGIN_TYPES",
+    "EmbedBehavior",
+    "EMBED_BEHAVIOR",
+    "RenderResult",
+    "RenderContext",
+    "RENDER_CAPABILITIES",
+    "EMBED_RULES",
+    "render_to",
+    "can_render",
+    "can_embed",
+    "get_embed_format",
+    "get_embed_behavior",
+    "register_embed_behavior",
+]
